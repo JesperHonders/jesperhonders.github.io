d26ed68ecb31adb1405ff04c5264558756531497
JesperHonders <jesper.honders@hva.nl>
2016-02-03 20:33:37 +0100

    het werkt. soort van

--- web-app-from-scratch/opdracht5/app.js
+++ web-app-from-scratch/opdracht5/app.js
@@ -4,26 +4,38 @@
 	var app = {
 		init:function() {
 			routes.init();
+            console.log("initialized")
 		}
 	};
 
 	var routes = {
 		init:function() {
 			window.addEventListener("hashchange", function(HashChangeEvent){
-				HashChangeEvent.newUrl
+                
+				var hashLocationNew = HashChangeEvent.newURL.search("#");
+                var hashLocationOld = HashChangeEvent.oldURL.search("#");
+                var newHash = HashChangeEvent.newURL.substr(hashLocationNew);
+                var oldHash = HashChangeEvent.oldURL.substr(hashLocationOld);
+                
+                sections.toggle(newHash, oldHash);
 			}, false);			
 		}
 	};
 
 	var sections = {
-		toggle: function (route) {
-			if(route === 'lijst') {
-
-			} else {
+		toggle: function (newHash, oldHash) {
+			if(newHash === '#list') {
+                document.getElementById("list").style.display = "block";
+                document.getElementById("home").style.display = "none";
+			}
+            else if(newHash === '#start'){
+                document.getElementById("home").style.display = "block";
+                document.getElementById("list").style.display = "none";
+            }
+            else {
 
 			}
 		}
 	}
-
 	app.init();
 }());
