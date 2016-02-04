25d03b33b4b37a3026be1142e2e1aa24b342a4a4
JesperHonders <jesper.honders@hva.nl>
2016-02-04 12:54:12 +0100

    Refactored & de-minified code

--- web-app-from-scratch/opdracht4/refactor.js
+++ web-app-from-scratch/opdracht4/refactor.js
@@ -109,12 +109,24 @@
     
 })();
 
+var debug = {
 
-// Event functies - bron: http://www.nczonline.net/blog/2010/03/09/custom-events-in-javascript/ Copyright (c) 2010 Nicholas C. Zakas. All rights reserved. MIT License
-// Gebruik: ET.addListener('foo', handleEvent); ET.fire('event_name'); ET.removeListener('foo', handleEvent);
-function EventTarget(){this._listeners={}}
-EventTarget.prototype={constructor:EventTarget,addListener:function(a,c){"undefined"==typeof this._listeners[a]&&(this._listeners[a]=[]);this._listeners[a].push(c)},fire:function(a){"string"==typeof a&&(a={type:a});a.target||(a.target=this);if(!a.type)throw Error("Event object missing 'type' property.");if(this._listeners[a.type]instanceof Array)for(var c=this._listeners[a.type],b=0,d=c.length;b<d;b++)c[b].call(this,a)},removeListener:function(a,c){if(this._listeners[a]instanceof Array)for(var b=
-    this._listeners[a],d=0,e=b.length;d<e;d++)if(b[d]===c){b.splice(d,1);break}}}; var ET = new EventTarget();
+    _geo_error_handler: function(code, message) {
+    debug_message('geo.js error '+code+': '+message);
+    },
+    
+    message: function(message){
+        (customDebugging && debugId)?document.getElementById(debugId).innerHTML:console.log(message);
+    },
+        
+    set_custom_debugging: function(debugId){
+        debugId = this.debugId;
+        customDebugging = true;
+    }
+    
+}
+
+
 
 // Test of GPS beschikbaar is (via geo.js) en vuur een event af
 
@@ -134,14 +146,33 @@ EventTarget.prototype={constructor:EventTarget,addListener:function(a,c){"undefi
  *  @param canvasID:string - het id van het HTML element waar de
  *      kaart in ge-rendered moet worden, <div> of <canvas>
  */
-function generate_map (myOptions, canvasId) {
-// TODO: Kan ik hier asynchroon nog de google maps api aanroepen? dit scheelt calls
-    debug_message("Genereer een Google Maps kaart en toon deze in #"+canvasId)
-    map = new google.maps.Map(document.getElementById(canvasId), myOptions);
 
-    var routeList = [];
-    // Voeg de markers toe aan de map afhankelijk van het tourtype
-    debug_message("Locaties intekenen, tourtype is: "+tourType);
+var googleMaps = {
+    
+    routeList : [],
+    
+    generate_map: function(myOptions, canvasId) {
+        // TODO: Kan ik hier asynchroon nog de google maps api aanroepen? dit scheelt calls
+        debug_message("Genereer een Google Maps kaart en toon deze in #"+canvasId)
+        map = new google.maps.Map(document.getElementById(canvasId), myOptions);
+    },
+
+}
+
+// Voeg de locatie van de persoon door
+    currentPositionMarker = new google.maps.Marker({
+        position: kaartOpties.center,
+        map: map,
+        icon: positieMarker,
+        title: 'U bevindt zich hier'
+    });
+
+    // Zorg dat de kaart geupdated wordt als het POSITION_UPDATED event afgevuurd wordt
+    ET.addListener(POSITION_UPDATED, update_positie);
+
+ // Voeg de markers toe aan de map afhankelijk van het tourtype
+    debug.message("Locaties intekenen, tourtype is: "+tourType);
+    
     for (var i = 0; i < locaties.length; i++) {
 
         // Met kudos aan Tomas Harkema, probeer local storage, als het bestaat, voeg de locaties toe
@@ -182,18 +213,6 @@ function generate_map (myOptions, canvasId) {
 
     }
 
-    // Voeg de locatie van de persoon door
-    currentPositionMarker = new google.maps.Marker({
-        position: kaartOpties.center,
-        map: map,
-        icon: positieMarker,
-        title: 'U bevindt zich hier'
-    });
-
-    // Zorg dat de kaart geupdated wordt als het POSITION_UPDATED event afgevuurd wordt
-    ET.addListener(POSITION_UPDATED, update_positie);
-}
-
 function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
@@ -201,21 +220,6 @@ function isNumber(n) {
 
 // FUNCTIES VOOR DEBUGGING
 
-var debug = {
 
-    function _geo_error_handler(code, message) {
-    debug_message('geo.js error '+code+': '+message);
-    },
-    
-    function debug_message(message){
-        (customDebugging && debugId)?document.getElementById(debugId).innerHTML:console.log(message);
-    },
-        
-    function set_custom_debugging(debugId){
-        debugId = this.debugId;
-        customDebugging = true;
-    }
-    
-}
 
 
