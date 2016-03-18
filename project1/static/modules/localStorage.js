734331138830eeedcb34b94f51d53d20cc6ab4ca
JesperHonders <jesper.honders@hva.nl>
2016-03-18 13:07:22 +0100

    Checks if Localstorage works for history panel

--- project1/static/modules/localStorage.js
+++ project1/static/modules/localStorage.js
@@ -1,4 +1,16 @@
-var seen = {
+function lsTest(){
+    var test = 'test';
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+if(lsTest() === true){
+   var seen = {
 	display: function(){
 		var data = localStorage.getItem('houses');
 		var house = JSON.parse(data);
@@ -36,3 +48,10 @@ var seen = {
 		
 	}
 }
+   console.log("Localstorage works, history panel enabled")
+   
+}else{
+	document.getElementById('historieButton').style.display = "none";
+	console.log("Localstorage doesn't work, History panel disabled")
+}
+
